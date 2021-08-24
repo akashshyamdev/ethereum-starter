12da8e6b2a705f970dc76c2619b042e3f790784d
Akash Shyam <shyamiy@gmail.com>
2021-08-24 19:11:37 +0530

    👷 chore: updated config for compiler

--- compile.js
+++ compile.js
@@ -5,4 +5,20 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, './contracts/Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(solc.compile(source, 1));
+const input = {
+	language: 'Solidity',
+	sources: {
+		'Inbox.sol': {
+			content: source,
+		},
+	},
+	settings: {
+		outputSelection: {
+			'*': {
+				'*': ['*'],
+			},
+		},
+	},
+};
+
+console.log(JSON.parse(solc.compile(JSON.stringify(input))));
