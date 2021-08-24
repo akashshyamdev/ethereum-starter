d0b03381df46782f8da60560fd3d96151b1f3887
Akash Shyam <shyamiy@gmail.com>
2021-08-24 18:44:01 +0530

    ✨ feat: inbox contract

---
+++ contracts/Inbox.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+contract Inbox {
+    string public message;
+    
+    constructor (string memory initialMessage) public {
+        message = initialMessage;
+    }
+    
+    function setMessage(string memory newMessage) public{
+        message = newMessage;
+    }
+}
